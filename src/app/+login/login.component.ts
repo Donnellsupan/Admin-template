73f9467700fbd1bc08b5035c490e5a073682a80e
Donnell <donnellsupan@gmail.com>
2019-06-08 22:25:10 +0800

    integrate login page

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   signIn() {
+    this.router.navigate(["/"]);
     localStorage.setItem("profile", "user");
+    
+    //this.router.onSameUrlNavigation = "reload";
+
+    setTimeout(() => {
+      
+      window.location.reload();
+      
+      //this.router.onSameUrlNavigation = "reload";
+    }, 1);
+    //window.location.reload();
+    //this.router.navigate(["/"])
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from 'angular-admin-lte';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,39 @@ import { LayoutService } from 'angular-admin-lte';
 })
 export class AppComponent implements OnInit {
   public customLayout: boolean;
+  isAuthenticated: boolean;
 
   constructor(
+    private router: Router,
     private layoutService: LayoutService
-  ) {}
+  ) { }
 
   ngOnInit() {
-    this.layoutService.isCustomLayout.subscribe((value: boolean) => {
-      console.log("dsfd", value)
-      this.customLayout = value;
-    });
+    // this.customLayout = true;
+    // this.router.navigate(["/"]);
+    //
+    if (localStorage.getItem("profile") === null) {
+      console.log(true)
+      this.customLayout = true;
+      this.router.navigate(["/login"]);
+    } else {
+      console.log("false")
+      this.customLayout = false;
+
+      //this.router.navigate(["/"]);
+      
+
+
+      // this.layoutService.isCustomLayout.subscribe((value: boolean) => {
+      //   console.log("dsfd", localStorage.getItem("profile"))
+      //   this.customLayout = value;
+      //   //this.isAuthenticated = true;
+      //   // if(localStorage.getItem("profile") === null){
+      //   // this.router.navigate(["/home"]);
+      //   // }
+      // });
+    }
+
+
   }
 }
