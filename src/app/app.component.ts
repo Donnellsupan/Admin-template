0eeaf3194b1ee654b1f8f2cf82472b292ebc56d6
Donnell <donnellsupan@gmail.com>
2019-06-08 17:32:04 +0800

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'angular-admin-lte';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  public customLayout: boolean;
+
+  constructor(
+    private layoutService: LayoutService
+  ) {}
+
+  ngOnInit() {
+    this.layoutService.isCustomLayout.subscribe((value: boolean) => {
+      console.log("dsfd", value)
+      this.customLayout = value;
+    });
+  }
+}
